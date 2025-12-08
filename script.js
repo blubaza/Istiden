a3cc4bf7f6241ed7632b88a2738d217c2990ba7c
blubaza <eliasjoostvestergaard@gmail.com>
2025-12-08 08:47:16 +0000

    Initial website

---
+++ script.js
@@ -0,0 +1,5 @@
+function showInfo(region) {
+    alert("Du klikkede på: " + region + 
+          "\n\nHer formede gletsjere landskabet ved at skubbe jord, sten og sand foran sig. " +
+          "Moræner, bakker og store sten er spor fra istiden.");
+}
